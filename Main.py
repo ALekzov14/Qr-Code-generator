c3b2a14d61f62d51c0d981fdd89126ef7245818f
ALekzov <151780073+ALekzov14@users.noreply.github.com>
2024-11-09 18:31:36 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import time
+import qrcode
+from Settings import QRbackcolor, QRfillcolor,QRboxsize, QRborder, QRversion
+
+check = "png"
+print("Hi!")
+time.sleep(0.8)
+# Основная часть / Main
+while True:
+    data = input("Enter the link (for example https://github.com/ALekzov14): ")
+    QRname = input("Enter the file name (for example qrcode.png):")
+
+    if check not in QRname:
+        print("Mistake.You have not entered the png file resolution!\n")
+    else:
+        break
+
+# Create QR-code / Создание QR-кода
+qr = qrcode.QRCode(version = QRversion, box_size = QRboxsize, border = QRborder)
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create image / Создание изображения QR-кода
+img = qr.make_image(fill_color = QRfillcolor, back_color = QRbackcolor)
+img.save(QRname)
+
+time.sleep(0.8)
+
+print(f"QR code named {QRname} has been successfully created!")
+image = Image.open(f"{QRname}")
+image.show()
---
+++ Settings.py
@@ -0,0 +1,8 @@
+# Settings / Настройки
+
+QRbackcolor = "white" # And any other colors, such as white and so on / можете указать другие цвета,например черный
+QRfillcolor = "black" # Similar to the previous comment / аналогично предыдущему комментарию
+QRversion = 1 # Версия 1: 21 x 21 модулей, Версия 2: 25 x 25 модулейm Версия 3: 29 x 29 модулей
+            # Version 1: 21 x 21 modules, Version 2: 25 x 25 modules, Version 3: 29 x 29 modules
+QRboxsize = 10
+QRborder = 5
